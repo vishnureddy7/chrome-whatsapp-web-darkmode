58c4ce14476750200ffd14b49e73c200335ae1dc
Guru Vishnu Vardhan Reddy <guruvishnu.desireddy@redbus.com>
2020-06-30 09:26:39 +0530

    made default mode as dark

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,8 @@
-if(localStorage['whatsapp-web-mode']=="dark"){
+if(localStorage['whatsapp-web-mode']==undefined){
+  document.body.classList.add("dark");
+  localStorage['whatsapp-web-mode']="dark";
+}
+else if(localStorage['whatsapp-web-mode']=="dark"){
   document.body.classList.add("dark");
 }
 else{
